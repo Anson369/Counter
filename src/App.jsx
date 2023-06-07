607a3844031e5f20097fb679cea74c4610fd111c
anson <ansonathomas777@gmail.com>
2023-06-07 14:22:38 +0530

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,22 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  function increase(){
+    setCount(count+1)
+  }
+  function reset(){
+    setCount(0)
+  }
+  function decrease(){
+    setCount(count-1)
+  }
   return (
     <>
       <h1>Counter</h1>
+      <h1>{count}</h1>
+      <button onClick={increase}>Increase</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={decrease}>Decrease</button>
     </>
   )
 }
